b1427f7b0a1887f421bcd1488a401ffa79ba52c7
falsidge <tesetings@gmail.com>
2016-07-13 21:21:50 -0700

    Add a config to change prices and amounts and add items into the shop's inventory

--- StardewMenu/Main.cs
+++ StardewMenu/Main.cs
@@ -12,12 +12,17 @@ using Microsoft.Xna.Framework;
 namespace StardewMenu
 {
     public class Main : Mod
-    {
+    { 
+        public static MenuConfig ModConfig { get; private set; }
+        public static Dictionary<Item, int[]> itemsinstock { get; set; }
         public override void Entry(params object[] objects)
         {
+            ModConfig = new MenuConfig().InitializeConfig(BaseConfigPath);
             StardewModdingAPI.Events.MenuEvents.MenuChanged += MenuChanged;
+            StardewModdingAPI.Events.GameEvents.GameLoaded += updateStock;
             //StardewModdingAPI.Command.RegisterCommand("time","set time to 800").CommandFired += Time;
             //StardewModdingAPI.Command.RegisterCommand("d", "turn on debug").CommandFired += Deb;
+            StardewModdingAPI.Command.RegisterCommand("updateStock", "reloads config and updates stock").CommandFired += updateStock;
         }
         private void MenuChanged(object sender, EventArgsClickableMenuChanged e)
         {
@@ -26,36 +31,43 @@ namespace StardewMenu
                 StardewValley.Menus.ShopMenu a = (StardewValley.Menus.ShopMenu)Game1.activeClickableMenu;
                 if (a.portraitPerson.getName() == "Clint" && a.potraitPersonDialogue != Game1.parseText("I can upgrade your tools with more power. You'll have to leave them with me for a few days, though.", Game1.dialogueFont, Game1.tileSize * 5 - Game1.pixelZoom * 4))
                 {
-                    Dictionary<Item, int[]> dictionary = new Dictionary<Item, int[]>();
-                    dictionary.Add((Item)new StardewValley.Object(Vector2.Zero, 378, int.MaxValue), new int[2]
-                    {
-        75,
-        int.MaxValue
-                    });
-                    dictionary.Add((Item)new StardewValley.Object(Vector2.Zero, 380, int.MaxValue), new int[2]
-                    {
-        150,
-        int.MaxValue
-                    });
-                    dictionary.Add((Item)new StardewValley.Object(Vector2.Zero, 382, int.MaxValue), new int[2]
-                    {
-        150,
-        int.MaxValue
-                    });
-                    dictionary.Add((Item)new StardewValley.Object(Vector2.Zero, 384, int.MaxValue), new int[2]
-                    {
-        400,
-        int.MaxValue
-                    });
-                    dictionary.Add((Item)new StardewValley.Object(Vector2.Zero, 386, int.MaxValue), new int[2]
-{
-        400,
-        int.MaxValue
-});
-                    Game1.activeClickableMenu = (IClickableMenu)new ItemM(dictionary, 0, "Clint");
-
+                    Game1.activeClickableMenu = (IClickableMenu)new ItemM(itemsinstock, 0, "Clint");
                 }
             }
+        }
+        public void updateStock()
+        {
+
+            itemsinstock = new Dictionary<Item, int[]>();
+            ModConfig = ModConfig.ReloadConfig();
+            for (int i = 0; i < ModConfig.stock.Count; i++)
+            {
+                Boolean errored = false;
+                Dictionary<string, int> sitem = ModConfig.stock[i];
+                if (!sitem.ContainsKey("cost"))
+                {
+                    Log.Error("Index " + i + " : is missing cost");
+                    errored = true;
+                }
+                if (!sitem.ContainsKey("id"))
+                {
+                    Log.Error("Index " + i + " : is missing id");
+                    errored = true;
+                }
+                if (!sitem.ContainsKey("amount"))
+                {
+                    Log.Error("Index " + i + " : is missing amount");
+                    errored = true;
+                }
+                if (!errored)
+                {
+                  itemsinstock.Add((Item)new StardewValley.Object(Vector2.Zero, sitem["id"], sitem["amount"] == 0 ? int.MaxValue : 1), new int[2] { sitem["cost"], sitem["amount"] == 0 ? int.MaxValue : sitem["amount"] });
+                }
+            }
+        }
+        public void updateStock(object sender, EventArgs e)
+        {
+            this.updateStock();
         }
         /*private void Time(object o, EventArgsCommand e)
         {
---
+++ StardewMenu/MenuConfig.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StardewModdingAPI;
+using StardewValley;
+using Microsoft.Xna.Framework;
+namespace StardewMenu
+{
+    public class MenuConfig : Config
+    {
+        public List<Dictionary<string,int>> stock { get; set;}
+        public override T GenerateDefaultConfig<T>()
+        {
+            stock = new List<Dictionary<string, int>>();
+            stock.Add(new Dictionary<string, int>() { {"id",378 }, { "amount",0}, { "cost", 75 } });
+            stock.Add(new Dictionary<string, int>() { { "id", 380 }, { "amount", 0 }, { "cost", 150 } });
+            stock.Add(new Dictionary<string, int>() { { "id", 382 }, { "amount", 0 }, { "cost", 150 } });
+            stock.Add(new Dictionary<string, int>() { { "id", 384 }, { "amount", 0 }, { "cost", 400 } });
+            stock.Add(new Dictionary<string, int>() { { "id", 386 }, { "amount", 0 }, { "cost", 750 } });
+           // Dictionary<Item, int[]> dictionary = new Dictionary<Item, int[]>();
+
+            return this as T;
+        }
+    }
+}
